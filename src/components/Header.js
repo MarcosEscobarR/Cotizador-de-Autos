e0cc709fe781c8cc16e9125d8b2ea0f3c0485fad
Marcos Escobar <marcosmaer600@gmail.com>
2020-06-15 10:14:17 -0400

    se cargo el proyecto y se agregaron los styles components a la pagina principal

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from "@emotion/styled"
+
+const ComponenteHeader = styled.header`
+    background-color: #26C6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #ffffff
+`;
+const CTexto = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`;
+
+const Header = ({titulo}) => {
+    return (
+        <ComponenteHeader> 
+            <CTexto>{titulo}</CTexto>
+        </ComponenteHeader>
+    )
+}
+
+export default Header;
